7c902d08798fcd04daafda907f88dfcbe2d7c3ad
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-02-28 03:25:32 -0600

    after a few tweaks

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -4,10 +4,15 @@ namespace Turtles
 {
     public static class Program
     {
+        
         public static void Main()
         {
             // TODO: Prints "I like turtles." 1000 times.
+            for (int counter = 0; counter < 1000; counter++)
+            {
 
+                Console.WriteLine("I like turtles.");
+            }
             Console.ReadLine();
         }
     }
